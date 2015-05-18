1193aa347885cacc5e7b5278d66c3fca3a65c0e2
maliciamrg <malicia.mrg@gmail.com>
2015-05-18 14:51:00 +0200

    ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 var myfonction =  function(e) { 
 	switch (e.target.innerText)
 	{
@@ -35,11 +36,29 @@ var myfonction =  function(e) {
 //		alert(this.innerText);
 };
 
-var myArraySerie = ['s1','s2','s3','s4'];
-for (var i = 0, c = myArraySerie.length; i < c; i++)
-{ 
-	var row = document.createElement("TR");
-	row.appendChild(document.createTextNode(myArraySerie[i]));
-	row.addEventListener('click', myfonction,  false); 
-	document.getElementById("menu").appendChild(row);
-}
+var Action = encodeURIComponent("ListeSerie"),
+    Key = encodeURIComponent("null");
+    
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://home.daisy-street.fr/ajax.php');  //?action=' + Action + '&key=' + Key);
+xhr.addEventListener('readystatechange', function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+    	alert(xhr.readyState+"-"+xhr.status+"-"+xhr.getResponseHeader('Content-type'));
+    	var myArraySerie = xhr.responseXML.getElementsByTagName('nom');
+		//var myArraySerie = ['s1','s2','s3','s4'];
+		for (var i = 0, c = myArraySerie.length; i < c; i++)
+		{ 
+			var row = document.createElement("TR");
+			row.appendChild(document.createTextNode(myArraySerie[i]));
+			row.addEventListener('click', myfonction,  false); 
+			document.getElementById("menu").appendChild(row);
+		}
+    } else if (xhr.readyState == 4 && xhr.status != 200) { // En cas d'erreur !
+    
+        alert('Une erreur est survenue !\n\nCode :' + xhr.status + '\nTexte : ' + xhr.statusText);
+
+    }
+	alert('<span>' + xhr.responseText + '</span>');
+
+}, false);
+xhr.send(null);
