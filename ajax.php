8c9c46980792ae4fdf719dc48a8bbb3e3ec46e1d
maliciamrg <malicia.mrg@gmail.com>
2015-05-18 17:17:55 +0200

    ajax2

---
+++ ajax.php
@@ -0,0 +1,57 @@
+<?php
+$action = $_GET["action"];
+$key =  $_GET["key"];
+header('Content-type: text/xml'); 
+echo "<?xml version=\"1.0\"?>\n";
+
+
+
+echo "<".$action.">\n";
+echo "<".$key.">\n";
+echo "</" .$key.">\n";
+
+
+//on connecte a la BDD
+$dbhost="localhost";
+$dbuser="seriedownload";
+$dbpass="seriedownload";
+$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
+mysql_select_db("seriedownload",$dblink);
+ 
+
+//on lance la requete
+$query = "";
+switch ($action) {
+    case "InfoSerie":
+        $query = "SELECT * FROM series WHERE nom = '" . $key . "'";
+        break;
+    case "ListeSeries":
+        $query = "SELECT nom FROM series ";
+        break;
+    case "ListeEpisodes":
+        $query = "SELECT * FROM episodes WHERE serie = ''" . $key . "''";
+        break;
+    case "ListeHashs":
+        $query = "SELECT * FROM hash ";
+        break;
+    default:
+        break;
+}
+
+if ($query != ""){
+	//Mise en forme resultat
+	$result = mysql_query($query,$dblink) or die (mysql_error($dblink));
+	//On boucle sur le resultat
+	while ($row = mysql_fetch_array($result, MYSQL_NUM))
+	{
+		for ($i=0; $i<mysql_num_fields($result); $i++) {
+			$nomchamp = mysql_field_name($result,$i);
+			$text=preg_replace('/&(?!#?[a-z0-9]+;)/', '&amp;', $row[$i]);
+			echo "<".$nomchamp.">";
+			echo $text;
+			echo"</".$nomchamp.">\n";
+		}
+	}
+}
+
+echo "</".$action.">\n";
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 
 var myfonction =  function(e) { 
 	switch (e.target.innerText)
-	{
-		case"s1":
-			var myArrayEpisodes = ['ep1','ep2','ep3','ep4'];
-			break;
-		case"s2":
-			var myArrayEpisodes = ['ep1','ep2','ep3'];
-			break;
-		case"s3":
-			var myArrayEpisodes = ['ep1','ep2'];
-			break;
-		case"s4":
-			var myArrayEpisodes = ['ep1'];
-			break;
-
-	}
 	
+	var Action = encodeURIComponent("InfoSerie"),
+    Key = encodeURIComponent(e.target.innerText);
+    
+var xhrep = new XMLHttpRequest();
+xhr.open('GET', 'http://home.daisy-street.fr/ajax.php?action=' + Action + '&key=' + Key);
+xhrep.addEventListener('readystatechange', function() {
+    if (xhrep.readyState === 4 && xhrep.status === 200) {
+    	alert(xhrep.readyState+"-"+xhrep.status+"-"+xhrep.getResponseHeader('Content-type'));
+    	var myArraySerie = xhrep.responseXML.getElementsByTagName('nom');
+		//var myArraySerie = ['s1','s2','s3','s4'];
+		for (var i = 0, c = myArraySerie.length; i < c; i++)
+		{ 
+			var row = document.createElement("TR");
+			row.appendChild(document.createTextNode(myArraySerie[i].childNodes[0].nodeValue));
+			row.addEventListener('click', myfonction,  false); 
+			document.getElementById("menu").appendChild(row);
+		}
+    } else if (xhr.readyState == 4 && xhr.status != 200) { // En cas d'erreur !
+    
+        alert('Une erreur est survenue !\n\nCode :' + xhr.status + '\nTexte : ' + xhr.statusText);
+
+    }
+
+}, false);
+xhrep.send(null);
+
 	var asupp =document.getElementById("lstEp");
 	if(asupp != null){
 	asupp.parentNode.removeChild(asupp);
@@ -36,11 +47,11 @@ var myfonction =  function(e) {
 //		alert(this.innerText);
 };
 
-var Action = encodeURIComponent("ListeSerie"),
+var Action = encodeURIComponent("ListeSeries"),
     Key = encodeURIComponent("null");
     
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://home.daisy-street.fr/ajax.php');  //?action=' + Action + '&key=' + Key);
+xhr.open('GET', 'http://home.daisy-street.fr/ajax.php?action=' + Action + '&key=' + Key);
 xhr.addEventListener('readystatechange', function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
     	alert(xhr.readyState+"-"+xhr.status+"-"+xhr.getResponseHeader('Content-type'));
